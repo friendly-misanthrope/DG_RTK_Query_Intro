76ee4203e53b02f68c215b49e08a0165ef4b80b2
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-27 18:26:43 -0700

    destructured data and properties from useGetToDosQuery hook

--- src/features/todos/ToDoList.jsx
+++ src/features/todos/ToDoList.jsx
@@ -1,43 +1,51 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useGetToDosQuery } from "../api/apiSlice";
 
 const TodoList = () => {
-    const [newTodo, setNewTodo] = useState('')
+  const [newTodo, setNewTodo] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //addTodo
-        setNewTodo('')
-    }
+  // renamed data with 'toDos' alias, destructured props from custom query hook
+  const {
+    data: toDos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetToDosQuery();
 
-    const newItemSection =
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="new-todo">Enter a new todo item</label>
-            <div className="new-todo">
-                <input
-                    type="text"
-                    id="new-todo"
-                    value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
-                    placeholder="Enter new todo"
-                />
-            </div>
-            <button className="submit">
-                <FontAwesomeIcon icon={faUpload} />
-            </button>
-        </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewTodo("");
+  };
 
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
 
-    let content;
-    // Define conditional content
+  let content;
 
-    return (
-        <main>
-            <h1>Todo List</h1>
-            {newItemSection}
-            {content}
-        </main>
-    )
-}
-export default TodoList
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </main>
+  );
+};
+export default TodoList;
