edf035ea9863b07f58154b388f7dbf050aa1453b
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-27 18:02:10 -0700

    created API slice with base URL & /todos endpoint

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  endpoints: (builder) => {
+    getToDos: builder.query({
+      query: () => '/todos'
+    });
+  }
+});
